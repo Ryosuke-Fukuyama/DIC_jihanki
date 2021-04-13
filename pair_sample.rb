c1a39318069be8ea2e221bbcc875b8173056f89d
ryosuke fukuyama <79957766+Ryosuke-Fukuyama@users.noreply.github.com>
2021-04-13 21:56:47 +0900

    Add files via upload

--- pair_sample.rb
+++ pair_sample.rb
@@ -1,7 +1,10 @@
 # 稼働例
 # irb
-# require '/Users/ryosuke.f/workspace/pairwork/pair.rb'
+# require '/Users/ryosuke.f/workspace/pairwork/pair_sample.rb'
 # vm = VendingMachine.new   # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
+#
+#
+#
 # ⓪
 # vm.slot_money(100)        # 作成した自動販売機に100円を入れる
 # vm.current_slot_money     # 作成した自動販売機に入れたお金がいくらかを確認する（表示する）
@@ -21,46 +24,20 @@
 # vm.purchase(0~2)          # 購入するジュースを選択し、釣り銭を出力する。
 
 
-# 各クラスの統合・選択し購入する
-class VendingMachine
-  MONEY = [10, 50, 100, 500, 1000].freeze
-  def initialize
-    5.times { store Drink.cola }
-  end
-  # ジュースの情報を出力
-  def store_juice
-    puts @button
-  end
-  # 購入する
-  def purchase(button)
-    if @buttons[button][:stock] >= 1
-      if current_slot_money >= @buttons[button][:price]
-        @slot_money -= @buttons[button][:price]
-        @buttons[button][:stock] -= 1
-        @sale += @buttons[button][:price]
-        puts slot_money
-      end
-    end
-    if @button == (0 || 1  || 2)
-      puts 選択し直して下さい。
-    end
-  end
-end
-
 
 # ジュースの属性定義
-class juice
+class drink
   attr_reader :name, :price
-  def self.cola
-    self.new 120, :cola
+  def self.coke
+    self.new (120, :coke)
   end
   def self.redbull
-    self.new 200, :redbull
+    self.new (200, :red_bull)
   end
   def self.water
-    self.new 100, :water
+    self.new (100, :water)
   end
-  def initialize price, name
+  def initialize (price, name)
     @name = name
     @price = price
   end
@@ -74,43 +51,80 @@ end
 
 
 # お金の増減管理
-class slot_money
-  #一時的に入ってる
-  @slot_money = 0
-  #確定
-  @sale = 0
+class 	amount
+  def initialize
+    #一時的に入ってるキャッシュトレー
+    @slot_money = slot_money = 0
+    #確定キャッシュトレー
+    @sale = sale = 0
+    @MONEY = MONEY
+  end
   def insert(money)
     return puts money unless MONEY.include?(money)
-    @slot_money += money
+    slot_money += money
   end
   def current_slot_money
-    @slot_money
+    slot_money
   end
   def return_money
-    puts @slot_money
-    @slot_money = 0
+    puts slot_money
+    slot_money = 0
   end
   def sale_proceeds
-    puts @sale
+    puts sale
   end
 end
 
 
-# 在庫の増減管理
-class store
+
+class stocks
+  # 在庫管理(メニュー変更)
   def stock_juice(name, stock)
-    @button_(name) = (stock)
+    button(:name) = (stock).times { Drink.(name) }
+    buttons = button(:name)
+  end
+  def initialize(name)
+    @button(:name) = button_(name)
+    @buttons = buttons
   end
 end
+
+
 # 表示
-
-
 class display
-  @buttons.length.times do |i|
-    if @buttons[i][:stock] != 0
-      if current_slot_money >= @buttons[i][:price]
-        @buttons[i][:name]
-       end
-     end
+  # ジュースの情報を出力
+  def store_juice
+    puts buttons
+  end
+  # 購入可能なドリンクの取得
+  def choice
+    buttons.length.times do |i|
+      if buttons[i] != nil
+        if slot_money >= @buttons[i][:price]
+          return buttons[i][:name]
+        end
+      end
+    end
+  end
+end
+
+
+# 各クラスの統合・選択し購入する
+class VendingMachine
+  MONEY = [10, 50, 100, 500, 1000].freeze
+  def initialize
+    button_coke = 5.times { Drink.coke }
+  end
+  # 購入する
+  def purchase(name)
+    push = button(:name)
+    if push.length != nil
+      if slot_money >= push[:price]
+        slot_money -= push[:price]
+        push[:stock] -= 1
+        sale += push[:price]
+        puts slot_money
+      end
+    end
   end
 end
