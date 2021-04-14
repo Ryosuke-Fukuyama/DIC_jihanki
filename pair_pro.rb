0bc59431bbecdd42e5bbf85a675d87141c591fe5
ryosuke fukuyama <79957766+Ryosuke-Fukuyama@users.noreply.github.com>
2021-04-14 19:48:42 +0900

    Add files via upload

---
+++ pair_pro.rb
@@ -0,0 +1,82 @@
+# 稼働例
+# irb
+# require '/Users/ryosuke.f/workspace/pairwork/pair_pro.rb'
+# vm = VendingMachine.new   インスタンス化
+# vm.slot_money ()   金額を入れる(制限付)
+# vm.store(redbull, 5)  在庫追加(レッドブルを5本)
+# vm.store(water, 5)  在庫追加(水を5本)
+# vm.store_juice    在庫確認
+# vm.choice   購入可能商品の照会
+# vm.purchase()    商品名で購入
+# vm.current_slot_money    現在の投入金額
+# vm.sale_proceeds    売上
+# vm.return_money   投入金返却
+
+class Drink
+  attr_reader :name, :price
+  def self.coke
+    self.new 120, :coke
+  end
+  def self.redbull
+    self.new 200, :red_bull
+  end
+  def self.water
+    self.new 100, :water
+  end
+  def initialize price, name
+    @name = name
+    @price = price
+  end
+end
+
+class VendingMachine
+   MONEY = [10, 50, 100, 500, 1000].freeze
+   @slot_money = slot_money
+   @sale = sale
+   @coke = coke
+   @buttons = buttons
+   def initialize
+    slot_money = 0
+    sale = 0
+    coke = Drink.coke
+    coke[:stock] = 5
+    buttons = coke
+  end
+  def current_slot_money
+    slot_money
+  end
+  def insert(money)
+    # !数字以外のもの確認!
+    return puts money unless MONEY.include?(money)
+    slot_money += money
+  end
+  def return_money
+    slot_money
+    slot_money = 0
+  end
+  def sale_proceeds
+    sale
+  end
+  def store_juice
+    buttons
+  end
+  def store(name, value)
+    # hash = {name: drink.name, price: drink.price, stock: num}
+    buttons << (Drink.name[:stock] = value)
+  end
+  def choice
+    buttons.length.times do |i|
+      if ( buttons[i][:stock] >= 1 ) && ( current_slot_money >= buttons[i][:price] )
+        p  buttons[i][:name]
+      end
+    end
+  end
+  def purchase(name)
+    if ( name.values_at(:stock) >= 1 ) && ( current_slot_money >= name.values_at(:price) )
+      slot_money -= name.values_at(:price)
+      name.values_at(:stock) -= 1
+      sale += name.values_at(:price)
+      slot_money
+    end
+  end
+end
